b87cf820e6d9918d48022f75459f11f2a3e2a09c
Jianlog <alphaxie1988@gmail.com>
2022-06-22 12:14:31 +0800

    add junit testing

--- locustfile.py
+++ locustfile.py
@@ -1,22 +1,22 @@
-from locust import HttpUser, between, task
+# from locust import HttpUser, between, task
 
-class Voter(HttpUser):
-    weight = 1
-    wait_time = between(0.1, 1)
+# class Voter(HttpUser):
+#     weight = 1
+#     wait_time = between(0.1, 1)
 
-    @task
-    def tabs(self):
-        self.client.post('/', {'team': 'TABS'})
+#     @task
+#     def tabs(self):
+#         self.client.post('/', {'team': 'TABS'})
 
-    @task
-    def spaces(self):
-        self.client.post('/', {'team': 'SPACES'})
+#     @task
+#     def spaces(self):
+#         self.client.post('/', {'team': 'SPACES'})
 
 
-class Loader(HttpUser):
-    weight = 5
-    wait_time = between(0.1, 1)
+# class Loader(HttpUser):
+#     weight = 5
+#     wait_time = between(0.1, 1)
 
-    @task
-    def home(self):
-        self.client.get('/')
+#     @task
+#     def home(self):
+#         self.client.get('/')
